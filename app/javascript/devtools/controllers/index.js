92b456c5aa4748e9459ba2eac0610aeb16dcea8e
Maxime Souillat <maxime@beaucouplus.com>
2024-11-29 17:53:52 +0100

    rename frontend to javascript

--- app/javascript/devtools/controllers/index.js
+++ app/javascript/devtools/controllers/index.js
@@ -1,12 +1,11 @@
 import { application } from "./application";
-import { registerControllers } from "stimulus-vite-helpers";
 import RevealController from '@stimulus-components/reveal'
 import Clipboard from '@stimulus-components/clipboard'
 import Notification from '@stimulus-components/notification'
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 
 application.register('reveal', RevealController)
 application.register('clipboard', Clipboard)
 application.register('notification', Notification)
 
-const controllers = import.meta.glob("./**/*_controller.js", { eager: true });
-registerControllers(application, controllers);
+eagerLoadControllersFrom("controllers", application)
