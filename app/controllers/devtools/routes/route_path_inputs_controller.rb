aa864b83ae6835961c97c61633b1193effd25eaf
Maxime Souillat <maxime@beaucouplus.com>
2024-12-17 16:02:35 +0100

    test route_path_inputs_controller

--- app/controllers/devtools/routes/route_path_inputs_controller.rb
+++ app/controllers/devtools/routes/route_path_inputs_controller.rb
@@ -7,8 +7,14 @@ module Devtools
         route = Routes::Collection.find(params[:id])
 
         input_params = { route: route }
-        input_params.merge!(prefix: route.engine_info.helper_prefix) if params[:engine_prefix].present? && params[:engine_prefix] == "1"
-        input_params.merge!(suffix: "url") if params[:url_suffix].present? && params[:url_suffix] == "1"
+
+        if params[:engine_prefix].present? && params[:engine_prefix] == "1"
+          input_params.merge!(prefix: route.engine_info.helper_prefix)
+        end
+
+        if params[:url_suffix].present? && params[:url_suffix] == "1"
+          input_params.merge!(suffix: "url")
+        end
 
         render Routes::RouteDetails::RoutePathInput.new(**input_params)
       end
---
+++ test/controllers/routes/route_path_inputs_controller_test.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+module Devtools
+  module Routes
+    class RoutePathInputsControllerTest < ActionDispatch::IntegrationTest
+      include Devtools::Engine.routes.url_helpers
+
+      def setup
+        @route = Routes::Collection.all.first
+        @route_id = @route.id
+      end
+
+      test "render path with path suffix when no optional parameters" do
+        patch routes_route_path_input_path(@route_id), xhr: true
+
+        assert_response :success
+        assert_equal "text/html", @response.media_type
+        assert_match "#{@route.name}_path", response.body
+      end
+
+      test "render path with path suffix when url suffix enabled" do
+        patch routes_route_path_input_path(@route_id), params: { url_suffix: "1" }, xhr: true
+
+        assert_response :success
+        assert_equal "text/html", @response.media_type
+        assert_match "#{@route.name}_url", response.body
+      end
+
+      test "render path with engine prefix when engine prefix enabled" do
+        route = Routes::Collection.all.last
+        patch routes_route_path_input_path(route.id), params: { engine_prefix: "1" }, xhr: true
+
+        assert_response :success
+        assert_equal "text/html", @response.media_type
+
+        refute_nil route.engine_info.helper_prefix
+        assert_match route.engine_info.helper_prefix, response.body
+      end
+
+
+      test "render path with engine prefix and url suffix updates when both engine prefix and url suffix enabled" do
+        route = Routes::Collection.all.last
+
+        patch(
+          routes_route_path_input_path(route.id),
+          params: { engine_prefix: "1", url_suffix: "1" },
+          xhr: true
+        )
+
+        assert_response :success
+        assert_equal "text/html", @response.media_type
+        assert_match "#{route.engine_info.helper_prefix}.#{route.name}_url", response.body
+      end
+    end
+  end
+end
