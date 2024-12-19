03573c945858492148b2bab377c522e2d1a413ae
Maxime Souillat <maxime@beaucouplus.com>
2024-12-19 11:37:21 +0100

    Forbid the gem to be used in production

--- lib/devtools/engine.rb
+++ lib/devtools/engine.rb
@@ -6,6 +6,14 @@ module Devtools
   class Engine < ::Rails::Engine
     isolate_namespace Devtools
 
+    # rubocop:disable Layout/LineLength
+    initializer "devtools.environment_check" do
+      unless Rails.env.local?
+        raise "Devtools can only be used in your local environment. Please make sure that it is not included in production environment in your gemfile."
+      end
+    end
+    # rubocop:enable Layout/LineLength
+
     # Add views to autoload paths for Phlex
     config.autoload_paths << "#{root}/app/views"
     config.autoload_paths << "#{root}/app/views/layouts"
